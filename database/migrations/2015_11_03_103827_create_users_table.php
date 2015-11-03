290a04bfbceaee39a0dbf7842040c434085a5aa4
BePsvPT <freedom@csie.io>
2015-11-03 12:23:54 +0800

    Create users and certificates tables

---
+++ database/migrations/2015_11_03_103827_create_users_table.php
@@ -0,0 +1,67 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username', 32);
+            $table->string('password', 100);
+            $table->rememberToken();
+            $table->string('nickname', 32);
+            $table->string('email');
+            $table->char('social_security_number', 10)->default('_infoexam_');
+            $table->integer('gender')->unsigned()->nullable();
+            $table->integer('department')->unsigned()->nullable();
+            $table->integer('grade')->unsigned()->nullable();
+            $table->char('class', 1)->default('A');
+            $table->tinyInteger('test_count')->unsigned()->default(0);
+            $table->tinyInteger('passed_score')->unsigned()->nullable();
+            $table->timestamp('passed_at')->nullable();
+            $table->timestamps();
+
+            $table->unique('username');
+
+            $table->index('remember_token');
+            $table->index('nickname');
+            $table->index('email');
+            $table->index('social_security_number');
+            $table->index('gender');
+            $table->index('department');
+            $table->index('grade');
+            $table->index('class');
+            $table->index('test_count');
+            $table->index('passed_at');
+            $table->index('updated_at');
+
+            $table->foreign('gender')->references('id')->on('categories')->onUpdate('cascade');
+            $table->foreign('department')->references('id')->on('categories')->onUpdate('cascade');
+            $table->foreign('grade')->references('id')->on('categories')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign('users_gender_foreign');
+            $table->dropForeign('users_department_foreign');
+            $table->dropForeign('users_grade_foreign');
+        });
+
+        Schema::drop('users');
+    }
+}
---
+++ database/migrations/2015_11_03_120342_create_certificates_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCertificatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('certificates', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('category_id')->unsigned();
+            $table->tinyInteger('value')->unsigned()->nullable();
+            $table->timestamps();
+
+            $table->unique(['user_id', 'category_id']);
+
+            $table->index('value');
+
+            $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade');
+            $table->foreign('category_id')->references('id')->on('categories')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('certificates', function (Blueprint $table) {
+            $table->dropForeign('certificates_user_id_foreign');
+            $table->dropForeign('certificates_category_id_foreign');
+        });
+
+        Schema::drop('certificates');
+    }
+}
