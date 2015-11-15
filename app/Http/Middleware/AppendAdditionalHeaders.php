185125b592f47b62c4b1c7c77d3a3c7a43473e1f
BePsvPT <freedom@csie.io>
2015-11-16 00:39:54 +0800

    fix bug

--- app/Http/Middleware/AppendAdditionalHeaders.php
+++ app/Http/Middleware/AppendAdditionalHeaders.php
@@ -17,14 +17,14 @@ class AppendAdditionalHeaders
      */
     public function handle($request, Closure $next)
     {
-        if (env('WEBSITE_HTTPS', false) && app()->environment(['production', 'development']) && ! $request->secure()) {
-            return redirect()->secure($request->getRequestUri());
-        }
+        $headers = [
+            'X-Content-Type-Options' => 'nosniff',
+            'X-Frame-Options' => 'sameorigin',
+            'X-XSS-Protection' => '1; mode=block',
+        ];
 
-        $response = $next($request);
-
-        if ($request->secure()) {
-            $response->header('Strict-Transport-Security', 'max-age=15552000; preload');
+        if ($request->secure() || env('WEBSITE_HTTPS', false)) {
+            $headers['Strict-Transport-Security'] = 'max-age=15552000; preload';
 
             if (! is_null($pins = env('PUBLIC_KEY_PINS')) && strlen($pins) > 0) {
                 $publicKeyPins = '';
@@ -33,15 +33,19 @@ class AppendAdditionalHeaders
                     $publicKeyPins .= " pin-sha256=\"{$pin}\";";
                 }
 
-                $response->header('Public-Key-Pins', "{$publicKeyPins} max-age=600;");
+                $headers['Public-Key-Pins'] = "{$publicKeyPins} max-age=600;";
+            }
+
+            if (! $request->secure()) {
+                return redirect()->secure($request->getRequestUri(), 302, $headers);
             }
         }
 
-        $response->header('X-Content-Type-Options', 'nosniff');
+        /** @var $response \Illuminate\Http\Response */
 
-        $response->header('X-Frame-Options', 'sameorigin');
+        $response = $next($request);
 
-        $response->header('X-XSS-Protection', '1; mode=block');
+        $response->headers->add($headers);
 
         return $response;
     }
