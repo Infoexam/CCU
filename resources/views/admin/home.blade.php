cec68dbf3bf0278d7ac43c63fcac357acdf63dbd
BePsvPT <freedom@csie.io>
2015-11-18 22:11:08 +0800

    Add Vue.js

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,7 +3,6 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta charset="UTF-8">
-        <title>資訊能力測驗管理頁面</title>
 
         <!-- Loading css sources -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
@@ -15,5 +14,6 @@
         <!-- Loading js sources -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js" defer></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/js/materialize.min.js" defer></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.8/vue.js" defer></script>
     </body>
 </html>
