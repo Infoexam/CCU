ba88800e092fcbc4b0a9ba6b9d36f0175b6d090e
BePsvPT <freedom@csie.io>
2015-11-23 11:13:06 +0800

    Update locale detect structure

--- app/Http/Middleware/PreprocessConnection.php
+++ app/Http/Middleware/PreprocessConnection.php
@@ -7,13 +7,14 @@ use Carbon\Carbon;
 use Closure;
 use Cookie;
 use Hash;
+use Illuminate\Http\Request;
 
 class PreprocessConnection
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  Request  $request
      * @param  \Closure  $next
      * @return mixed
      */
@@ -22,32 +23,57 @@ class PreprocessConnection
         // set the cost for BCRYPT to 12
         Hash::setRounds(12);
 
-        // set environment and carbon default language
-        $lan = 'en';
+        // 設定環境語言
+        $locale = $this->getLocale($request);
 
-        if ($request->has('lang')) {
-            $userAcceptLanguages = [strstr($request->input('lang'), '/', true)];
-        } else if ($request->hasCookie('locale')) {
-            $userAcceptLanguages = [$request->cookie('locale')];
-        } else {
-            $userAcceptLanguages = Agent::languages();
-        }
+        app()->setLocale($locale);
+        Carbon::setLocale($locale);
 
-        foreach (['zh-TW', 'zh-tw', 'zh'] as $zh) {
-            if (in_array($zh, $userAcceptLanguages)) {
-                $lan = 'zh-TW';
-            }
-        }
-
-        app()->setLocale($lan);
-        Carbon::setLocale($lan);
+        // 處理不支援的瀏覽器
 
         /** @var $response \Illuminate\Http\Response */
 
         $response = $next($request);
 
-        $response->withCookie(Cookie::forever('locale', $lan, null, null, false, false));
+        $response->withCookie(Cookie::forever('locale', $locale, null, null, false, false));
 
         return $response;
+    }
+
+    /**
+     * 取得環境語言
+     *
+     * @param Request $request
+     * @param string $defaultLocale
+     * @return string
+     */
+    protected function getLocale(Request $request, $defaultLocale = 'en')
+    {
+        $userAcceptLanguages = $this->getUserAcceptLanguages($request);
+
+        foreach (['zh-TW', 'zh-tw', 'zh'] as $zh) {
+            if (in_array($zh, $userAcceptLanguages)) {
+                return 'zh-TW';
+            }
+        }
+
+        return $defaultLocale;
+    }
+
+    /**
+     * 取得使用者偏好語言
+     *
+     * @param Request $request
+     * @return array
+     */
+    protected function getUserAcceptLanguages(Request $request)
+    {
+        if ($request->has('lang')) {
+            return [strstr($request->input('lang'), '/', true)];
+        } else if ($request->hasCookie('locale')) {
+            return [$request->cookie('locale')];
+        }
+
+        return Agent::languages();
     }
 }
