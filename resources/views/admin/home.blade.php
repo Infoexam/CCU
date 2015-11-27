0a59952f399c2266d995c093abb468ed886d119c
Eyi <sdses9001@gmail.com>
2015-11-27 15:22:27 +0800

    update template

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.master')
 
 @section('header')
-    <nav>
-        <div class="nav-wrapper indigo darken-3">
+    <nav class="indigo darken-3">
+        <div class="nav-wrapper container indigo darken-3">
             <!-- Logo -->
             <a v-link="{name: 'home'}" class="brand-logo">Logo Icon</a>
 
