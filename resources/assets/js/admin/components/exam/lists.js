aff5aec7716e278e521d8d36a71e7f2639e9e9d7
you11581 <gg4627942@gmail.com>
2015-12-02 00:39:05 +0800

    Add delete function

--- resources/assets/js/admin/components/exam/lists.js
+++ resources/assets/js/admin/components/exam/lists.js
@@ -3,7 +3,7 @@ routerComponents.exam.lists = {
         template: require('../../template/admin/exam/lists/index.html') ,
 
         data: function () {
-            return {sets: [], pagination: {}};
+            return {lists: [], pagination: {}};
         },
 
         methods: {
@@ -15,12 +15,24 @@ routerComponents.exam.lists = {
                     url = this.pagination[next ? 'next_page_url' : 'prev_page_url'];
                 }
                     this.$http.get(url, function (data, status, request) {
-                        this.$set('sets', data.data);
+                        this.$set('lists', data.data);
                         delete data.data;
                         this.$set('pagination', data);
                     });
             },
+
+            destroy: function (id, index) {
+                this.$http.delete('/api/v1/exam/lists/' + id, function (data, status, request) {
+                    Materialize.toast($.i18n.t('action.delete.success'), 3500, 'green');
+
+                    this.papers.splice(index, 1);
+                }).error(function (data, status, request) {
+                    Materialize.toast($.i18n.t('action.delete.failed'), 3500, 'red');
+                });
+            }
+
         },
+
 
         ready: function () {
             this.paginate();
@@ -32,12 +44,12 @@ routerComponents.exam.lists = {
         template: require('../../template/admin/exam/lists/show.html') , 
 
         data: function() {
-            return { sets: {} };
+            return { lists: {} };
         } , 
         ready: function() {
             var url = '/api/v1/exam/lists/' + this.$route.params.id;
             this.$http.get(url , function (data , status , request){
-                    this.$set('sets' , data);
+                    this.$set('lists' , data);
             });
         }
 
