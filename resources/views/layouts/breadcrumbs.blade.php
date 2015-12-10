66c60ae32b6be9503a8476cb1c291192a318b2de
BePsvPT <freedom@csie.io>
2015-12-10 23:28:02 +0800

    Fix oversight

---
+++ resources/views/layouts/breadcrumbs.blade.php
@@ -0,0 +1,7 @@
+<nav v-show="breadcrumbs.length">
+    <div class="nav-wrapper">
+        <div class="col s12">
+            <a v-for="breadcrumb in breadcrumbs" v-link="{name: breadcrumb.name, params: breadcrumb.params}" class="breadcrumb">@{{ breadcrumb.name }}</a>
+        </div>
+    </div>
+</nav>
