8b7b5884d8e4e76042d991db904049de9bce6c98
BePsvPT <freedom@csie.io>
2015-12-10 17:53:52 +0800

    Update app config and exclude dev packages from development environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (! $this->app->environment(['production'])) {
+        if (! $this->app->environment(['production', 'development'])) {
             $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
             $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
         }
