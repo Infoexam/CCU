f5480ec4fe25cbfeea1aaa0c58d477288456d9fc
BePsvPT <freedom@csie.io>
2015-12-15 21:33:00 +0800

    Transform mobile navigation from dropdown to collapsible

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,28 +1,11 @@
 @extends('layouts.master')
 
 @section('header')
-    <nav class="indigo darken-1">
-        <div class="nav-wrapper container indigo darken-1">
-            <!-- Logo -->
-            <a v-link="{name: 'home'}" class="brand-logo">Logo Icon</a>
+    <!-- Desktop Navigation -->
+    @include('admin.nav.desktop')
 
-            <!-- Mobile Menu Icon -->
-            <a v-link="{name: 'home'}" data-activates="nav-mobile-menu-icon" class="button-collapse"><i class="material-icons" @click.prevent>menu</i></a>
-
-            <!-- Desktop Menu -->
-            <ul class="right hide-on-med-and-down">
-                @include('admin._nav', ['device' => 'desktop'])
-            </ul>
-
-            <!-- Mobile Menu -->
-            <ul class="side-nav" id="nav-mobile-menu-icon">
-                @include('admin._nav', ['device' => 'mobile'])
-            </ul>
-        </div>
-    </nav>
-
-    @include('admin._nav_dropdown', ['device' => 'desktop'])
-    @include('admin._nav_dropdown', ['device' => 'mobile'])
+    <!-- Mobile Navigation -->
+    @include('admin.nav.mobile')
 @endsection
 
 @section('main')
