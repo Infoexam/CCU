27c4e149280b41a298cfd34de23c6f40f9bfee4b
Eyi <sdses9001@gmail.com>
2015-12-18 15:19:43 +0800

    update announcement templates

--- resources/views/layouts/breadcrumbs.blade.php
+++ resources/views/layouts/breadcrumbs.blade.php
@@ -1,7 +1,7 @@
 <nav class="breadnav transparent hide-on-small-only" v-show="breadcrumbs.length">
     <div class="nav-wrapper">
         <div class="col s12">
-            <span class="breadcrumb-navi material-icons blue-text text-darken-4">navigation</span>
+            <span class="breadcrumb-navi material-icons black-text">navigation</span>
             <a v-for="breadcrumb in breadcrumbs" v-link="{name: breadcrumb.name, params: breadcrumb.params}" class="breadcrumb blue-text">@{{ breadcrumb.name }}</a>
         </div>
     </div>
