1464e5d4ed704ee2139b37b2ea8a668302924131
BePsvPT <freedom@csie.io>
2015-12-25 23:58:04 +0800

    Update packages

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -36,7 +36,7 @@
         <!-- Loading JavaScript Sources -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js" defer></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.js" defer></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.12/vue.js" defer></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.13/vue.js" defer></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/vue-resource/0.5.1/vue-resource.min.js" defer></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/vue-router/0.7.7/vue-router.min.js" defer></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/i18next/1.11.1/i18next.min.js" defer></script>
