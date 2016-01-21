e070f33b6147193a36c16982d8fd3a213c20dcd6
Eyi <sdses9001@gmail.com>
2016-01-21 12:41:13 +0800

    Add student/nav.blade.php

---
+++ resources/views/student/nav.blade.php
@@ -0,0 +1,9 @@
+<nav class="student-nav">
+    <div class="container">
+        <!-- Logo -->
+        <a v-link="{name: 'home'}" class="brand-logo">Logo Icon</a>
+
+        <!-- Mobile Menu Icon -->
+        <a v-link="{name: 'home'}" data-activates="nav-mobile-menu-icon" class="button-collapse"><i class="material-icons" @click.prevent>menu</i></a>
+    </div>
+</nav>
