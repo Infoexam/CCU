3f9501c6550825339b5bd4cad86ae80b733084f3
Eyi <sdses9001@gmail.com>
2016-01-21 14:11:30 +0800

    Update templates

--- resources/views/student/footer.blade.php
+++ resources/views/student/footer.blade.php
@@ -1,6 +1,26 @@
 <div class="student-footer grey darken-2">
     <div class="container">
-        <span><a href="http://www.ccu.edu.tw/" target="_blank">{{ trans('navigation.copyright') }}</a> c 2015</span>
-        <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+        <div class="row">
+            <div class="col m6 l6">
+                <ul>
+                    <li><a class="grey-text text-lighten-4" href="http://www.ccu.edu.tw/" target="_blank">{{ trans('navigation.copyright') }} c 2015</a></li>
+                </ul>
+            </div>
+            <div class="col m3 l3">
+                <ul>
+                    <li><h5 class="grey-text text-lighten-4">使用說明</h5></li>
+                    <li><a class="grey-text text-lighten-4" href="#!"><i class="material-icons tiny" style="vertical-align:middle">label_outline</i>應考須知</a></li>
+                    <li><a class="grey-text text-lighten-4" href="#!"><i class="material-icons tiny" style="vertical-align:middle">label_outline</i>測驗規定</a></li>
+                    <li><a class="grey-text text-lighten-4" href="#!"><i class="material-icons tiny" style="vertical-align:middle">label_outline</i>測驗說明</a></li>
+                </ul>
+            </div>
+            <div class="col m3 l3">
+                <ul>
+                    <li><h5 class="grey-text text-lighten-4">相關連結</h5></li>
+                    <li><a class="grey-text text-lighten-4" href="#!">相關連結1</a></li>
+                    <li><a class="grey-text text-lighten-4" href="#!">相關連結2</a></li>
+                </ul>
+            </div>
+        </div>
     </div>
 </div>
