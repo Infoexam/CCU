1003c7b29c3b3688b011611f44c910b1895e0594
BePsvPT <freedom@csie.io>
2016-02-04 10:13:33 +0800

    Fix pushpin

--- resources/assets/js/general.js
+++ resources/assets/js/general.js
@@ -1,67 +1,70 @@
-"use strict";
-
 /* General Vanilla JS and jQuery */
 
 (function ($) {
-    $(function () {
-        // http://materializecss.com/pushpin.html
-        $('.feature-bar').pushpin({top: 94});
+  $(function () {
+    // http://materializecss.com/pushpin.html
+    $('.feature-bar').pushpin({top: 94});
 
-        // https://i18next.github.io/i18next/
-        $.i18n.init({
-            detectLngQS: 'lang',
-            cookieName: 'locale',
-            fallbackLng: false,
-            lngWhitelist: ['en', 'zh-TW'],
-            resGetPath: '/assets/locales/__ns__-__lng__.json'
-        }, function() {
-            $('[data-i18n]').i18n();
-        });
-
-        /*
-         * https://github.com/uzairfarooq/arrive
-         */
-
-        $(document).arrive('[data-i18n]', function () {
-            $(this).i18n();
-        });
-
-        // http://materializecss.com/navbar.html
-        $(document).arrive('.button-collapse', function () {
-            $(this).sideNav();
-        });
-
-        // http://materializecss.com/dialogs.html#tooltip
-        $(document).arrive('.tooltipped', function () {
-            $(this).tooltip({delay: 50});
-        });
-        $(document).leave('.tooltipped', function () {
-            $(this).tooltip('remove');
-        });
-
-        // http://materializecss.com/forms.html#character-counter
-        $(document).arrive('input[length], textarea[length]', function () {
-            $(this).characterCounter();
-        });
-
-        // http://materializecss.com/media.html#materialbox
-        $(document).arrive('.materialboxed', function () {
-            $(this).materialbox();
-        });
-
-        // http://materializecss.com/collapsible.html#intialization
-        $(document).arrive('.collapsible', function () {
-            $(this).collapsible();
-        });
-
-        $(document).arrive('.modal-trigger', function () {
-            $(this).leanModal();
-        });
+    // https://i18next.github.io/i18next/
+    $.i18n.init({
+      detectLngQS: 'lang',
+      cookieName: 'locale',
+      fallbackLng: false,
+      lngWhitelist: ['en', 'zh-TW'],
+      resGetPath: '/assets/locales/__ns__-__lng__.json'
+    }, function() {
+      $('[data-i18n]').i18n();
     });
+
+    /*
+     * https://github.com/uzairfarooq/arrive
+     */
+
+    $(document).arrive('[data-i18n]', function () {
+      $(this).i18n();
+    });
+
+    // http://materializecss.com/pushpin.html
+    $(document).arrive('.feature-bar', function () {
+      $(this).pushpin({top: 94});
+    });
+
+    // http://materializecss.com/navbar.html
+    $(document).arrive('.button-collapse', function () {
+      $(this).sideNav();
+    });
+
+    // http://materializecss.com/dialogs.html#tooltip
+    $(document).arrive('.tooltipped', function () {
+      $(this).tooltip({delay: 50});
+    });
+    $(document).leave('.tooltipped', function () {
+      $(this).tooltip('remove');
+    });
+
+    // http://materializecss.com/forms.html#character-counter
+    $(document).arrive('input[length], textarea[length]', function () {
+      $(this).characterCounter();
+    });
+
+    // http://materializecss.com/media.html#materialbox
+    $(document).arrive('.materialboxed', function () {
+      $(this).materialbox();
+    });
+
+    // http://materializecss.com/collapsible.html#intialization
+    $(document).arrive('.collapsible', function () {
+      $(this).collapsible();
+    });
+
+    $(document).arrive('.modal-trigger', function () {
+      $(this).leanModal();
+    });
+  });
 })(jQuery);
 
 if ('localhost' === window.location.hostname) {
-    Vue.config.debug = true;
+  Vue.config.debug = true;
 }
 
 // add X-XSRF-TOKEN to xhr
@@ -69,5 +72,5 @@ if ('localhost' === window.location.hostname) {
 Vue.http.headers.common['X-XSRF-TOKEN'] = decodeURIComponent(('; ' + document.cookie).split('; XSRF-TOKEN=').pop().split(';').shift());
 
 var router = new VueRouter(),
-    routerComponents = {exam: {}},
-    routerParentInstance = Vue.extend({template: '<router-view></router-view>'});
+  routerComponents = {exam: {}},
+  routerParentInstance = Vue.extend({template: '<router-view></router-view>'});
