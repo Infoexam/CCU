2e25363b56a34bf04e5646b71bad7544cf6ecad1
BePsvPT <freedom@csie.io>
2016-05-02 11:38:28 +0800

    Remove old code

--- resources/views/admin.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.master')
-
-@section('scripts')
-    <script src="{{ _asset('js/admin.js') }}" defer></script>
-@endsection
--- resources/views/student.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.master')
-
-@section('scripts')
-    <script src="{{ _asset('js/student.js') }}" defer></script>
-@endsection
