6d6026315d34f3f482045e48aff2e2fb2252e8fc
BePsvPT <freedom@csie.io>
2016-05-02 18:19:48 +0800

    Update packages and gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,39 @@
-let elixir = require('laravel-elixir');
+var elixir = require('laravel-elixir')
+var webpack = require('webpack')
 
-require('laravel-elixir-vueify');
+require('laravel-elixir-webpack')
 
 if (elixir.config.production) {
-  elixir.config.publicPath = 'public/assets';
+  elixir.config.publicPath = 'public/assets'
 }
 
-elixir.config.notifications = false;
+elixir.config.notifications = false
 
 elixir(function (mix) {
-  mix.browserify('main.js');
-});
+  mix.webpack('main.js', {
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          loader: 'babel',
+          query: {
+            presets: ['es2015']
+          },
+          exclude: /node_modules/
+        },
+
+        { test: /\.vue$/, loader: 'vue' },
+        { test: /\.woff2?$|\.ttf$|\.eot$|\.svg$/, loader: 'file' },
+        { test: /\.scss$/, loaders: ['style', 'css', 'resolve-url', 'sass?sourceMap'] },
+        { test: /\.css$/, loaders: ['style', 'css'] },
+        { test: /\.(png|jpg|gif)$/, loader: 'file' }
+      ],
+
+      plugins: [
+        new webpack.ProvidePlugin({
+          $: 'jquery'
+        })
+      ]
+    }
+  })
+})
