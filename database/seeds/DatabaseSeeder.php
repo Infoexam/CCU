a991e3ffc1024391e7dcc6c3c9f04b3e1a679605
BePsvPT <freedom@csie.io>
2016-05-13 11:28:20 +0800

    WIP

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,10 +12,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(CategoriesTableSeeder::class);
-        $this->call(WebsiteRelatedTablesSeeder::class);
+//        $this->call(WebsiteRelatedTablesSeeder::class);
         $this->call(UsersTableSeeder::class);
-        $this->call(ExamSetsTableSeeder::class);
-        $this->call(PapersTableSeeder::class);
-        $this->call(ExamListsTableSeeder::class);
+//        $this->call(ExamSetsTableSeeder::class);
+//        $this->call(PapersTableSeeder::class);
+//        $this->call(ExamListsTableSeeder::class);
     }
 }
