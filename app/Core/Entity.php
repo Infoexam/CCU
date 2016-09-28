fd15f558078fde981abd0e2826507598b3d5ffde
BePsvPT <freedom@csie.io>
2016-09-28 21:21:03 +0800

    Increment version

--- app/Core/Entity.php
+++ app/Core/Entity.php
@@ -12,7 +12,7 @@ abstract class Entity extends Eloquent
     /**
      * Application version.
      */
-    const VERSION = '0.0.9';
+    const VERSION = '0.0.10';
 
     /**
      * The number of models to return for pagination.
