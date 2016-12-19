decc249e8bc53b634fafcbeb58b5b109bca07fe2
BePsvPT <freedom@csie.io>
2016-12-19 22:47:28 +0800

    Encrypt users password

---
+++ database/migrations/2016_12_19_224157_encrypt_users_table_password_column.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class EncryptUsersTablePasswordColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::table('users')->where('version', 1)->get()->each(function ($user) {
+            DB::table('users')->where('id', $user->id)->update(['password' => encrypt($user->password)]);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::table('users')->where('version', 1)->get()->each(function ($user) {
+            DB::table('users')->where('id', $user->id)->update(['password' => decrypt($user->password)]);
+        });
+    }
+}
