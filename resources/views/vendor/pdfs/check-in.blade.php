2a3fe34867899c89d8867793b6f194b438e77c3e
BePsvPT <freedom@csie.io>
2016-12-27 11:53:21 +0800

    Update check-in template

--- resources/views/vendor/pdfs/check-in.blade.php
+++ resources/views/vendor/pdfs/check-in.blade.php
@@ -12,66 +12,69 @@
 
       thead, tbody, tr, th, td {
         border: 1px solid black;
+        page-break-inside: avoid;
       }
 
       th, td {
-        padding: .45rem .7rem;
+        padding: .7rem .5rem;
       }
     </style>
   </head>
   <body>
-    <table style="text-align: left;">
-      <tbody>
-        <tr>
-          <td>場次：{{ $listing->getAttribute('code') }}</td>
-          <td>地點：{{ $listing->getAttribute('room') }}</td>
-          <td>人數：{{ $listing->getAttribute('applied_num') }} 人</td>
-        </tr>
-        <tr>
-          <td colspan="3">
-            類型：{{ trans('infoexam.exam.subject.'.$listing->getRelation('subject')->getAttribute('name')) }}　{{ trans('infoexam.exam.apply.'.$listing->getRelation('applyType')->getAttribute('name')) }}
-          </td>
-        </tr>
-        <tr>
-          <td colspan="3">時間：{{ $listing->getAttribute('began_at') }}　{{ $listing->getAttribute('duration') }} 分鐘</td>
-        </tr>
-      </tbody>
-    </table>
-
-    <br>
-
-    <table>
-      <thead style="display: table-header-group">
-        <tr>
-          <th>#</th>
-          <th>學號</th>
-          <th>姓名</th>
-          <th>簽到</th>
-          <th>備註</th>
-          <th>#</th>
-          <th>學號</th>
-          <th>姓名</th>
-          <th>簽到</th>
-          <th>備註</th>
-        </tr>
-      </thead>
-      <tbody>
-        @foreach($listing->getRelation('applies')->chunk(2) as $chunk)
-          <tr style="page-break-inside: avoid">
-            @foreach ($chunk as $index => $apply)
-              <td style="padding: .45rem .3rem;">{{ $index + 1 }}</td>
-              <td>{{ $apply->getRelation('user')->getAttribute('username') }}</td>
-              <td>{{ $apply->getRelation('user')->getAttribute('name') }}</td>
-              <td>　　　</td>
-              <td>
-                @if (is_null($apply->getAttribute('paid_at')))
-                  <span>未繳費</span>
-                @endif
-              </td>
-            @endforeach
+    @foreach($listing->getRelation('applies')->chunk(40) as $chunks)
+      <table style="text-align: left;">
+        <tbody>
+          <tr>
+            <td>場次：{{ $listing->getAttribute('code') }}</td>
+            <td>地點：{{ $listing->getAttribute('room') }}</td>
+            <td>人數：{{ $listing->getAttribute('applied_num') }} 人</td>
           </tr>
-        @endforeach
-      </tbody>
-    </table>
+          <tr>
+            <td colspan="3">
+              類型：{{ trans('infoexam.exam.subject.'.$listing->getRelation('subject')->getAttribute('name')) }}　{{ trans('infoexam.exam.apply.'.$listing->getRelation('applyType')->getAttribute('name')) }}
+            </td>
+          </tr>
+          <tr>
+            <td colspan="3">時間：{{ $listing->getAttribute('began_at') }}　{{ $listing->getAttribute('duration') }} 分鐘</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <br>
+
+      <table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>學號</th>
+            <th>姓名</th>
+            <th>簽到</th>
+            <th>備註</th>
+            <th>#</th>
+            <th>學號</th>
+            <th>姓名</th>
+            <th>簽到</th>
+            <th>備註</th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach($chunks->chunk(2) as $chunk)
+            <tr>
+              @foreach ($chunk as $index => $apply)
+                <td style="padding: .45rem .3rem;">{{ $index + 1 }}</td>
+                <td>{{ $apply->getRelation('user')->getAttribute('username') }}</td>
+                <td>{{ $apply->getRelation('user')->getAttribute('name') }}</td>
+                <td>　　　　</td>
+                <td>
+                  @if (is_null($apply->getAttribute('paid_at')))
+                    <span>未繳費</span>
+                  @endif
+                </td>
+              @endforeach
+            </tr>
+          @endforeach
+        </tbody>
+      </table>
+    @endforeach
   </body>
 </html>
