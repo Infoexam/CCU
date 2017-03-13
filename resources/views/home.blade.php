d28e8f02a96538c7ce559a65e3c4358a1449de61
BePsvPT <freedom@csie.io>
2017-03-13 15:00:37 +0800

    Build

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,15 +9,15 @@
     <link rel="stylesheet" href="{{ _asset('css/font-awesome.min.css') }}" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossOrigin="anonymous">
     <link rel="stylesheet" href="{{ _asset('css/flexboxgrid.min.css') }}" integrity="sha384-lYQsy3+y5OZ48NxBmwaDdLIfraac06B9Y+GxLBpls0w3B97eRObW0Bs6yzuUMvV9" crossOrigin="anonymous">
     <link rel="stylesheet" href="{{ _asset('css/materialize.min.css') }}" integrity="sha384-b53qKoC8m5cqqbOrHihG/FABYWjs4RCLaQpiSD5/6gKXmsKJDk0Cth0CVGWqiH1+" crossOrigin="anonymous">
-    <link rel="stylesheet" href="{{ _asset('js/main.css') }}" integrity="sha384-n/D4CGg8rdH+l4B7ni0enlIYupZIc5AZTMD6vwyKToSvR2PbmloehIvnzdfdPbw/ sha384-IYBRoy5uhZ259GcUObJ+ZKQuqRm8pfdZlYFvPbtzT+zwP/1BOqqoe7ds2RqB0hlr" crossOrigin="anonymous">
+    <link rel="stylesheet" href="{{ _asset('js/main.css') }}" integrity="sha384-YPAqFqExZnrCHLvHn+eluYtIoBmOwFdTpLf5L6yaqgVYobCxT2GHVlpXBALz3gEA sha384-IYBRoy5uhZ259GcUObJ+ZKQuqRm8pfdZlYFvPbtzT+zwP/1BOqqoe7ds2RqB0hlr" crossOrigin="anonymous">
   </head>
   <body>
     <main></main>
 
     <script src="{{ _asset('js/jquery.min.js') }}" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossOrigin="anonymous" defer></script>
     <script src="{{ _asset('js/materialize.min.js') }}" integrity="sha384-MtG4F5bFIlwRS0MZiDIhuZp/FjoKpMFITxaG2vZqu3K6uh8Xx0VYgpo7JZ4uTJaT" crossOrigin="anonymous" defer></script>
-    <script src="{{ _asset('js/manifest.js') }}" integrity="sha384-/D3AGYPHOG62N83IIOYsJ2lYIInATz01gDy9/j27QKytBPdwyHBU4jWFIXGMoTot sha384-yCYDb1zpQsaLY4xtYLD8u/a8zApva1Q3H1YZ/oVVNoQZ7SQUqmHJV7LjF0mxc6vR" crossOrigin="anonymous" defer></script>
-    <script src="{{ _asset('js/vendor.js') }}" integrity="sha384-cC8Qtc/wZ62CIg2Jmh/K78kdyv5L6aLAI2z/wl6AFK6a4DR0afOPW0s6LgUlMDd2 sha384-L6apBCqZG+lz25bcxlkIe9J9JZtNtHm61wZ2LfHL8bw+67X6Y3n22alSIZe5vWFl" crossOrigin="anonymous" defer></script>
-    <script src="{{ _asset('js/main.js') }}" integrity="sha384-4bRWbS31subKvKc9iuJjkipUvI++gIbk98GKx6V4SYZ2K2Ts2hS/23WaNzWkWefu sha384-jBrAEcOSedO4PqHvyWOhSIfph0pjm6flFkeTD/OTy1zJiPS/mMeB2CtP/5RknoHR" crossOrigin="anonymous" defer></script>
+    <script src="{{ _asset('js/manifest.js') }}" integrity="sha384-ya1MBrEns5dH8MnwsMOgDvfbNzbUK5N4vLKC7OlmkwLqwMEgHhzZZdqkeCc9KLx9 sha384-yCYDb1zpQsaLY4xtYLD8u/a8zApva1Q3H1YZ/oVVNoQZ7SQUqmHJV7LjF0mxc6vR" crossOrigin="anonymous" defer></script>
+    <script src="{{ _asset('js/vendor.js') }}" integrity="sha384-9p2meUXk1mbViAdiQfcy1Va+1ofO08Lov/i7iQATy4/wsXItGg1dJO4CFLj8xUmn sha384-L6apBCqZG+lz25bcxlkIe9J9JZtNtHm61wZ2LfHL8bw+67X6Y3n22alSIZe5vWFl" crossOrigin="anonymous" defer></script>
+    <script src="{{ _asset('js/main.js') }}" integrity="sha384-8e8ezY/87hpIXwvRDgzXqv0zRhk0CIYT22Kh2+LfHi4a/Z2/3x/3beEQ3JlJJMiG sha384-jBrAEcOSedO4PqHvyWOhSIfph0pjm6flFkeTD/OTy1zJiPS/mMeB2CtP/5RknoHR" crossOrigin="anonymous" defer></script>
   </body>
 </html>
