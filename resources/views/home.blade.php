7014ea1ae3998d652b66c9c01977968487106eaf
Vincent <vincent0740@gmail.com>
2018-03-17 00:07:13 +0800

    modify practice view content

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,20 +4,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta charset="UTF-8">
-    <title>資訊能力測驗系統</title>
+    <title>資訊能力測驗系統1</title>
     <link rel="stylesheet" href="{{ _asset('css/material-icons.css') }}" integrity="sha384-HW5BS5ZexnkLWD0V9LWRI4qqfF+jY5czO55nfMCI8ZYZ2v+eQgO0jT8OuHPRXiJ2" crossOrigin="anonymous">
     <link rel="stylesheet" href="{{ _asset('css/font-awesome.min.css') }}" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossOrigin="anonymous">
     <link rel="stylesheet" href="{{ _asset('css/flexboxgrid.min.css') }}" integrity="sha384-lYQsy3+y5OZ48NxBmwaDdLIfraac06B9Y+GxLBpls0w3B97eRObW0Bs6yzuUMvV9" crossOrigin="anonymous">
     <link rel="stylesheet" href="{{ _asset('css/materialize.min.css') }}" integrity="sha384-b53qKoC8m5cqqbOrHihG/FABYWjs4RCLaQpiSD5/6gKXmsKJDk0Cth0CVGWqiH1+" crossOrigin="anonymous">
-    <link rel="stylesheet" href="{{ _asset('js/main.css') }}" integrity="sha384-hpZh7kiO03fhrictpIzrht4kvYtMFLS86cSIQRtVuo77vZIGKS/XOM6u+3CoEnKi sha384-Lv8+Y/47vGw7iHWiDM7zFqZbhSG3WnxztQSKIcBdoZB526/ZQ+C+RirvEHTVb9h1" crossOrigin="anonymous">
+    <link rel="stylesheet" href="{{ _asset('js/main.css') }}" integrity="sha384-o/YUd0VjAIcplBKCU/ohHPhADT/Kr0XmSd7UV+CQCDe+IMXuIalYO0WZaK3l1vF1" crossOrigin="anonymous">
   </head>
   <body>
     <main></main>
 
     <script src="{{ _asset('js/jquery.min.js') }}" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossOrigin="anonymous" defer></script>
     <script src="{{ _asset('js/materialize.min.js') }}" integrity="sha384-MtG4F5bFIlwRS0MZiDIhuZp/FjoKpMFITxaG2vZqu3K6uh8Xx0VYgpo7JZ4uTJaT" crossOrigin="anonymous" defer></script>
-    <script src="{{ _asset('js/manifest.js') }}" integrity="sha384-ya1MBrEns5dH8MnwsMOgDvfbNzbUK5N4vLKC7OlmkwLqwMEgHhzZZdqkeCc9KLx9 sha384-yCYDb1zpQsaLY4xtYLD8u/a8zApva1Q3H1YZ/oVVNoQZ7SQUqmHJV7LjF0mxc6vR" crossOrigin="anonymous" defer></script>
-    <script src="{{ _asset('js/vendor.js') }}" integrity="sha384-x+w19WA6Al/sqD1IHf4Syg1Xt7++ZIjqzd0Bfj0texA9pWVWERldX4eAKEqOr2SU sha384-LjEJZgqpGE7WT59gdb5OCjPRBf0VMuE1V/918ZpebOD+wQGrM4k0ZvF6RGw3Gk+L" crossOrigin="anonymous" defer></script>
-    <script src="{{ _asset('js/main.js') }}" integrity="sha384-ww+92ANw5gkn7V3I4+T2Vvjj+JbkeDvuJ9upwmwEqQsO1ORssSX2DFQvikSUvA92 sha384-iWmemdRUeRa1XdusUp09d785PBWaG2RFjN0idYQDSfNRIQedvgI1jeNMeXhgivQl" crossOrigin="anonymous" defer></script>
+    <script src="{{ _asset('js/manifest.js') }}" integrity="sha384-20HybGmnl0sgyzIkmc0HOI3rlc3NBIQsRtw/wIgAiRmpeqCwQ0SP/HSjyIhypser" crossOrigin="anonymous" defer></script>
+    <script src="https://static-infoexam.ccu.edu.tw/assets/0.0.22/js/vendor.js')" integrity="sha384-HncTACYHnHPW1mTGIyaBPpp1UYkCfxxZuCqHflEG8Wv3/j5oxQFnhaQ4kpUYhybC" crossOrigin="anonymous" defer></script>
+    <script src="https://static-infoexam.ccu.edu.tw/assets/0.0.22/js/main.js')" integrity="sha384-gN5GtT5wuDWXll2w4/CrzphVoqP/uiCyblr3jkqEWu7GP8lJqOc+3peEGOTkCZOo" crossOrigin="anonymous" defer></script>
   </body>
 </html>
