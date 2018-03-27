298b5b7dd7b6bde177ecd1acc1b741c7c353c1b4
Vincent Huang <vincent0740@gmail.com>
2018-03-27 09:43:43 -0700

    update integrity

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,17 +5,17 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta charset="UTF-8">
     <title>資訊能力測驗系統</title>
-    <link rel="stylesheet" href="{{ _asset('css/material-icons.css') }}" integrity="sha384-LLRcxNa5elvFu5Iym7JYhi9uC348veTAbs3W8W/LeMAAscvT2oZC/cdemCeuq2ay" crossOrigin="anonymous">
-    <link rel="stylesheet" href="{{ _asset('css/font-awesome.min.css') }}" integrity="sha384-aJb1qS3I4QnLhRo7PnB30F9VFxyRk6tR7Fh1y4a656NR3gZbyoFkiDcUo53Nb9Y9" crossOrigin="anonymous">
+    <link rel="stylesheet" href="{{ _asset('css/material-icons.css') }}" integrity="sha384-HW5BS5ZexnkLWD0V9LWRI4qqfF+jY5czO55nfMCI8ZYZ2v+eQgO0jT8OuHPRXiJ2" crossOrigin="anonymous">
+    <link rel="stylesheet" href="{{ _asset('css/font-awesome.min.css') }}" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossOrigin="anonymous">
     <link rel="stylesheet" href="{{ _asset('css/flexboxgrid.min.css') }}" integrity="sha384-lYQsy3+y5OZ48NxBmwaDdLIfraac06B9Y+GxLBpls0w3B97eRObW0Bs6yzuUMvV9" crossOrigin="anonymous">
-    <link rel="stylesheet" href="{{ _asset('css/materialize.min.css') }}" integrity="sha384-SySZjpSWOOsoU4hWmPopN6KhH1l7Ocg0ufCwJwzWKN/zHiFpRMMYjMYT+YQ0aG5R" crossOrigin="anonymous">
+    <link rel="stylesheet" href="{{ _asset('css/materialize.min.css') }}" integrity="sha384-b53qKoC8m5cqqbOrHihG/FABYWjs4RCLaQpiSD5/6gKXmsKJDk0Cth0CVGWqiH1+" crossOrigin="anonymous">
     <link rel="stylesheet" href="{{ _asset('js/main.css') }}" integrity="sha384-hzd4aKnszaY01/jI7DcS8jiYTrZQTalJd7Bn9jf8ATJU0Bf3ZI3mmIph/s7Tf5dg sha384-+UeYLF4DrzsuBGLzARKiTsEXgCfdedIpAyW79A0HuG5CweAXW+7yCYxoSur5LIEz" crossOrigin="anonymous">
   </head>
   <body>
     <main></main>
 
-    <script src="{{ _asset('js/jquery.min.js') }}" integrity="sha384-5Ru+es+1eJ6FZtb9JCp5uYPCBE19zmF82FPgibn70CGE9WcLPASBsyFMyiD5nRw1" crossOrigin="anonymous" defer></script>
-    <script src="{{ _asset('js/materialize.min.js') }}" integrity="sha384-+8V0Dkccp3WuuOf/ghfczZ++zl76wGJ7qQizJcgzQ/uiTAyFwiyTyBISzDsv3AVj" crossOrigin="anonymous" defer></script>
+    <script src="{{ _asset('js/jquery.min.js') }}" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossOrigin="anonymous" defer></script>
+    <script src="{{ _asset('js/materialize.min.js') }}" integrity="sha384-MtG4F5bFIlwRS0MZiDIhuZp/FjoKpMFITxaG2vZqu3K6uh8Xx0VYgpo7JZ4uTJaT" crossOrigin="anonymous" defer></script>
     <script src="{{ _asset('js/manifest.js') }}" integrity="sha384-ya1MBrEns5dH8MnwsMOgDvfbNzbUK5N4vLKC7OlmkwLqwMEgHhzZZdqkeCc9KLx9 sha384-yCYDb1zpQsaLY4xtYLD8u/a8zApva1Q3H1YZ/oVVNoQZ7SQUqmHJV7LjF0mxc6vR" crossOrigin="anonymous" defer></script>
     <script src="{{ _asset('js/vendor.js') }}" integrity="sha384-x+w19WA6Al/sqD1IHf4Syg1Xt7++ZIjqzd0Bfj0texA9pWVWERldX4eAKEqOr2SU sha384-vmZUYEW4d/SPZxSkCxFETizNIzqxlBrHsyglwKsBgeo1xYSGsPTMTsm/HqMsU+dE" crossOrigin="anonymous" defer></script>
     <script src="{{ _asset('js/main.js') }}" integrity="sha384-vXmDd2ygCGNhTFswbkEmwRgMRSo9RvPb1nri35W6m1ioW8vq/b002jSnbJ7peG6L sha384-w/zbmZwsKj0OZXDETrp5f4dN10lnWt99mLWkqi53M9FZHVkMY1pqF1VbO/iTjt/A" crossOrigin="anonymous" defer></script>
