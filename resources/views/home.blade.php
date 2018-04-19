ebdd88eb609b16598b5f62410bc53509803bd0e4
Vincent Huang <vincent0740@gmail.com>
2018-04-19 11:19:19 -0700

    view content modify

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="{{ _asset('css/font-awesome.min.css') }}" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossOrigin="anonymous">
     <link rel="stylesheet" href="{{ _asset('css/flexboxgrid.min.css') }}" integrity="sha384-lYQsy3+y5OZ48NxBmwaDdLIfraac06B9Y+GxLBpls0w3B97eRObW0Bs6yzuUMvV9" crossOrigin="anonymous">
     <link rel="stylesheet" href="{{ _asset('css/materialize.min.css') }}" integrity="sha384-b53qKoC8m5cqqbOrHihG/FABYWjs4RCLaQpiSD5/6gKXmsKJDk0Cth0CVGWqiH1+" crossOrigin="anonymous">
-    <link rel="stylesheet" href="{{ _asset('js/main.css') }}" integrity="sha384-hzd4aKnszaY01/jI7DcS8jiYTrZQTalJd7Bn9jf8ATJU0Bf3ZI3mmIph/s7Tf5dg sha384-+UeYLF4DrzsuBGLzARKiTsEXgCfdedIpAyW79A0HuG5CweAXW+7yCYxoSur5LIEz" crossOrigin="anonymous">
+    <link rel="stylesheet" href="{{ _asset('js/main.css') }}" integrity="sha384-fah/dhxxRX3XG56dZltX+HEcxRfEbbLYoX90wlD2Ywqgwrxm3XqVf6/N2kqo9ySZ sha384-oWI89+LzbKf/nPzsykLhVxnVHVolClcgh/WElK7MYLw1bzA3goD//ZSUcfxjQT5y" crossOrigin="anonymous">
   </head>
   <body>
     <main></main>
@@ -18,6 +18,6 @@
     <script src="{{ _asset('js/materialize.min.js') }}" integrity="sha384-MtG4F5bFIlwRS0MZiDIhuZp/FjoKpMFITxaG2vZqu3K6uh8Xx0VYgpo7JZ4uTJaT" crossOrigin="anonymous" defer></script>
     <script src="{{ _asset('js/manifest.js') }}" integrity="sha384-ya1MBrEns5dH8MnwsMOgDvfbNzbUK5N4vLKC7OlmkwLqwMEgHhzZZdqkeCc9KLx9 sha384-yCYDb1zpQsaLY4xtYLD8u/a8zApva1Q3H1YZ/oVVNoQZ7SQUqmHJV7LjF0mxc6vR" crossOrigin="anonymous" defer></script>
     <script src="{{ _asset('js/vendor.js') }}" integrity="sha384-x+w19WA6Al/sqD1IHf4Syg1Xt7++ZIjqzd0Bfj0texA9pWVWERldX4eAKEqOr2SU sha384-vmZUYEW4d/SPZxSkCxFETizNIzqxlBrHsyglwKsBgeo1xYSGsPTMTsm/HqMsU+dE" crossOrigin="anonymous" defer></script>
-    <script src="{{ _asset('js/main.js') }}" integrity="sha384-vXmDd2ygCGNhTFswbkEmwRgMRSo9RvPb1nri35W6m1ioW8vq/b002jSnbJ7peG6L sha384-w/zbmZwsKj0OZXDETrp5f4dN10lnWt99mLWkqi53M9FZHVkMY1pqF1VbO/iTjt/A" crossOrigin="anonymous" defer></script>
+    <script src="{{ _asset('js/main.js') }}" integrity="sha384-A18SVjzEJE9SD/iyVQtCz5Q6czaUXlN69qy75jMi3l2wWarUuvSx7kDlTS2vBGs2 sha384-0/4kkPNiFZve3d7vPVizqBRL6k26fNygMoHP8kJqtrQT9mu+tIVXo2qP4VaFvDh/" crossOrigin="anonymous" defer></script>
   </body>
 </html>
